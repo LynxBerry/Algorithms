b12d408633e1691d16150d45b9fbebf10ebb986f
Steven Shao <stevenshao1900@icloud.com>
2016-06-07 20:20:22 +0800

    added

---
+++ Sum100/Sum100.ts
@@ -0,0 +1,49 @@
+//print all the combination 1,2...9 
+// 1 + 2 + 3 + 45 + 6 + 7 + 8 + 9
+
+//It is not best solution.
+function sum100(n: number, path: string, op: string):void {
+    if ( n == 9) {
+        if(eval(path + "+" + n.toString()) == 100)
+        {
+            console.log(path + "+" + n.toString());
+        }
+        
+        if(eval(path + "-" + n.toString()) == 100)
+        {
+            console.log(path + "-" + n.toString());
+        }
+        
+        if(eval(path + n.toString()) == 100)
+        {
+            console.log(path + n.toString());
+        }
+        
+        return;
+
+    } else {
+        if (n == 1) {
+            sum100(n + 1, "1", "");
+
+        } else {
+            if (eval(path + "+" + n.toString()) < 100)
+            {
+                sum100(n + 1, path + "+" + n.toString(), "+");
+            };
+
+            if(eval(path + n.toString()) < 100)
+            {
+                sum100(n + 1, path + n.toString(), "");
+
+            }
+
+            sum100(n + 1, path + "-" + n.toString(), "-");
+            
+
+        }
+
+    }
+
+}
+
+sum100(1,"","");

3f7fbb63a746e3e456ad42f177537b235d26156e
LynxBerry <stevenshao1900@icloud.com>
2016-06-08 17:36:41 +0800

    added

--- NQueens/NQueen.ts
+++ NQueens/NQueen.ts
@@ -1,5 +1,28 @@
 // https://leetcode.com/problems/n-queens/
 function solveNQueens(n:number):string[][] {
+    let hashUsedSpots = {};
+    let freeColumns = [];
+    let freeRows = [];
+
+    for(let i = 0; i < n; i ++)
+    {
+        //pick up one column from free columns
+        //pick up one row from free rows
+        //put that queen onto the chess table at row, column
+
+
+    }
+
+    function getFreeSpot()
+    {
+
+    }
+
+    function placeQueen(row: number, column: number) {
+
+    }
+
+
 
 
 }

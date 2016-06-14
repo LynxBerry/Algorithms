177949180b3446463f3c73428662241201f579cb
LynxBerry <stevenshao1900@icloud.com>
2016-06-14 14:38:03 +0800

    added duplicate cases.

--- IntersectionOfTwoArrays/intersectArrays.js
+++ IntersectionOfTwoArrays/intersectArrays.js
@@ -17,4 +17,24 @@ var intersection = function (nums1, nums2) {
     return arrayIntersect;
 };
 console.log(intersection([1, 2, 2, 1], [2, 2]));
+// https://leetcode.com/problems/intersection-of-two-arrays-ii/
+// version 2 - allow duplicates
+var intersect = function (nums1, nums2) {
+    var hash = {};
+    var arrayIntersect = [];
+    var lengthNums1 = nums1.length;
+    var lengthNums2 = nums2.length;
+    for (var i = 0; i < lengthNums1; i++) {
+        hash[nums1[i]] = (!hash[nums1[i]]) ? 1 : (hash[nums1[i]] + 1); // cannot use 0 because 0 and undefined are the same.
+    }
+    //console.log(hash);
+    for (var i = 0; i < lengthNums2; i++) {
+        if (hash[nums2[i]] && (hash[nums2[i]] > 0)) {
+            arrayIntersect.push(nums2[i]);
+            hash[nums2[i]]--;
+        }
+    }
+    return arrayIntersect;
+};
+console.log(intersect([1, 2, 2, 1, 2, 2], [2, 2, 2]));
 //# sourceMappingURL=intersectArrays.js.map
--- IntersectionOfTwoArrays/intersectArrays.ts
+++ IntersectionOfTwoArrays/intersectArrays.ts
@@ -24,5 +24,28 @@ let intersection = function (nums1: number[], nums2: number[]) {
 
 console.log(intersection([1,2,2,1], [2,2]));
 
+// https://leetcode.com/problems/intersection-of-two-arrays-ii/
+// version 2 - allow duplicates
+let intersect = function(nums1: number[], nums2: number[]):number[] {
+    let hash:any = {};
+    let arrayIntersect = [];
+    let lengthNums1 = nums1.length;
+    let lengthNums2 = nums2.length;
+    for (let i = 0; i < lengthNums1; i++) {
+        hash[nums1[i]] = (!hash[nums1[i]])? 1 : (hash[nums1[i]] + 1) ; // cannot use 0 because 0 and undefined are the same.
+    }
+    //console.log(hash);
 
+    for (let i =0; i < lengthNums2; i++) {
+        if (hash[nums2[i]] && (hash[nums2[i]] > 0)) { //for first time occurence.
+            arrayIntersect.push(nums2[i]);
+            hash[nums2[i]]--;
+        }
+    }
+
+    return arrayIntersect;
+
+};
+
+console.log(intersect([1,2,2,1,2,2], [2,2,2]));
 

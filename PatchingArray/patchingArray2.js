e244ba2f2e6be3b78825d186a6678f19a60ef830
LynxBerry <stevenshao1900@icloud.com>
2016-06-16 17:40:42 +0800

    modified.

--- PatchingArray/patchingArray2.js
+++ PatchingArray/patchingArray2.js
@@ -19,7 +19,7 @@ var minPatches2 = function (nums, n) {
             }
             i++;
         }
-        //console.log(copyNums);
+        console.log(copyNums);
         return patchCount;
     }
     return null;
@@ -32,19 +32,17 @@ console.log(minPatches2([1, 2, 31, 33], 2147483647));
 */
 // faster faster solution
 var minPatches3 = function (nums, n) {
-    var copyNums = nums.slice(0);
     var patchCount = 0;
     if (n > 0) {
         var i = 0;
         var sum = 0; //starting from 0;
         while (true) {
-            if ((!copyNums[i]) || (sum + 1 < copyNums[i])) {
-                //copyNums.splice(i, 0, sum + 1);
+            if ((!nums[i]) || (sum + 1 < nums[i])) {
                 sum += (sum + 1);
                 patchCount++;
             }
             else {
-                sum += copyNums[i];
+                sum += nums[i];
                 i++;
             }
             if (sum >= n) {
@@ -56,8 +54,62 @@ var minPatches3 = function (nums, n) {
     }
     return null;
 };
-console.log("test2");
+/*
+console.log("test3");
 console.log(minPatches3([], 7));
 console.log(minPatches3([1, 5, 10], 20));
 console.log(minPatches3([1, 2, 31, 33], 2147483647));
+*/
+// faster faster faster solution
+var minPatches4 = function (nums, n) {
+    var patchCount = 0;
+    if (n > 0) {
+        var i = 0;
+        var sum = 0; //starting from 0; still needs sum.
+        while (true) {
+            if (!nums[i]) {
+                if (sum !== 0) {
+                    patchCount += Math.ceil(Math.log(n / sum) / Math.log(2));
+                    break;
+                }
+                patchCount += Math.floor(Math.log(n) / Math.log(2)) + 1;
+                break;
+            }
+            else if (n <= nums[i]) {
+                if (sum === 0) {
+                    patchCount += Math.ceil(Math.log(n) / Math.log(2));
+                }
+                else {
+                    patchCount += Math.ceil(Math.log(n / sum) / Math.log(2));
+                }
+                break;
+            }
+            else if (nums[i] < n) {
+                if (sum <= nums[i]) {
+                    var count = 0;
+                    if (sum === 0) {
+                        count = Math.ceil(Math.log(nums[i]) / Math.log(2));
+                        patchCount += count;
+                    }
+                    else {
+                        count = Math.ceil(Math.log(nums[i] / sum) / Math.log(2));
+                        patchCount += count;
+                    }
+                    sum = sum * Math.pow(2, count) + nums[i] + 1;
+                }
+                else {
+                    sum += nums[i] + 1;
+                }
+                i++;
+            }
+        }
+        //console.log(copyNums);
+        return patchCount;
+    }
+    return null;
+};
+console.log("test4");
+//console.log(minPatches4([1, 2, 31, 33], 2147483647));
+//console.log(minPatches4([1, 5, 10], 20));
+console.log(minPatches4([1, 2, 3], 2147483647)); // 29
 //# sourceMappingURL=patchingArray2.js.map
--- PatchingArray/patchingArray2.ts
+++ PatchingArray/patchingArray2.ts
@@ -17,7 +17,7 @@ let minPatches2 = function(nums: number[], n: number): number {
             if (sum >= n) { break; }
             i++;
         }
-        //console.log(copyNums);
+        console.log(copyNums);
         return patchCount;
 
     }
@@ -25,6 +25,7 @@ let minPatches2 = function(nums: number[], n: number): number {
 
     
 }
+
 /*
 console.log("test");
 console.log(minPatches2([], 7));
@@ -34,18 +35,16 @@ console.log(minPatches2([1, 2, 31, 33], 2147483647));
 
 // faster faster solution
 let minPatches3 = function(nums: number[], n: number): number {
-    let copyNums = nums.slice(0);
     let patchCount = 0;
     if (n > 0) {
         let i = 0;
         let sum = 0; //starting from 0;
         while(true) {
-            if ( (!copyNums[i]) || (sum + 1 < copyNums[i])) { // if next element not exist or sum + 1 < next element
-                //copyNums.splice(i, 0, sum + 1);
+            if ( (!nums[i]) || (sum + 1 < nums[i])) { // if next element not exist or sum + 1 < next element
                 sum += (sum + 1);
                 patchCount++;
             } else { // next element exist && sum + 1 >= next element
-                sum += copyNums[i];
+                sum += nums[i];
                 i++;
             }
             if (sum >= n) { break; }
@@ -59,7 +58,67 @@ let minPatches3 = function(nums: number[], n: number): number {
 
     
 }
-console.log("test2");
+/*
+console.log("test3");
 console.log(minPatches3([], 7));
 console.log(minPatches3([1, 5, 10], 20));
 console.log(minPatches3([1, 2, 31, 33], 2147483647));
+*/
+
+
+// faster faster faster solution
+let minPatches4 = function(nums: number[], n: number): number {
+    let patchCount = 0;
+    if (n > 0) {
+        let i = 0;
+        let sum = 0; //starting from 0; still needs sum.
+        while(true) {
+            if ( !nums[i] ) { // if next element not exist or sum + 1 < next element
+                if (sum !== 0) {
+                    patchCount += Math.ceil(Math.log(n/sum)/Math.log(2));
+                    break;
+                }
+                patchCount += Math.floor(Math.log(n)/Math.log(2)) + 1;
+                break;
+            } else if (n <= nums[i]) { // if nums[i] exists
+                    if (sum === 0) {
+                        patchCount += Math.ceil(Math.log(n)/Math.log(2));
+                    } else {
+                        patchCount += Math.ceil(Math.log(n/sum)/Math.log(2));
+                    }
+
+                    break;
+
+            } else if (nums[i] < n) {
+                if ( sum <= nums[i]) {
+                    let count = 0;
+                    if (sum === 0) {
+                        count = Math.ceil(Math.log(nums[i])/Math.log(2));
+                        patchCount += count;
+                    } else {
+                        count = Math.ceil(Math.log(nums[i]/sum)/Math.log(2));
+                        patchCount += count;
+                    }
+                    sum = sum  * Math.pow(2, count) + nums[i] + 1;
+
+                } else { // >=
+                    sum += nums[i] + 1;
+                }
+                i++;
+            }
+  
+
+        }
+        //console.log(copyNums);
+        return patchCount;
+
+    }
+    return null;
+
+    
+}
+
+console.log("test4");
+//console.log(minPatches4([1, 2, 31, 33], 2147483647));
+//console.log(minPatches4([1, 5, 10], 20));
+console.log(minPatches4([1,2,3],2147483647)); // 29

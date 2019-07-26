cf8f42681bc0aab64517daf17f7070814387ffdd
Steven Shao <stevensh@microsoft.com>
2019-07-26 17:18:59 +0800

    Init

---
+++ 2019_07_26_Heap/Heap/Heap/Heap.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heap
+{
+    class Heap
+    {
+        private int[] binaryTree;
+        private int countOfNodes;
+
+        private void swap(ref int a,ref int b)
+	    {
+            int temp = a;
+            a = b;
+		    b = temp;	
+	    }
+
+        public Heap(int[] binaryTree)
+        {
+            this.binaryTree = binaryTree;
+            countOfNodes = binaryTree.Length - 1; // the 0th element doesn't count.
+
+        }
+
+        public void Heapify()
+        {
+            // Start from the leaf nodes. Sift down process
+            for (int i = countOfNodes; i >= 1; i--)
+            {
+                int j = i;
+                while (2 * j <= countOfNodes)
+                {
+                    int indexOfMaxChildren = ((2 * j + 1 <= countOfNodes) && (binaryTree[2 * j + 1] > binaryTree[2 * j])) ? (2 * j + 1) : (2 * j);
+
+                    if (binaryTree[indexOfMaxChildren] > binaryTree[j])
+                    {
+                        swap(ref binaryTree[indexOfMaxChildren], ref binaryTree[j]);
+
+                        j = indexOfMaxChildren;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+            }
+
+
+        }
+
+        public void PrintHeap()
+        {
+            foreach (var n in binaryTree)
+            {
+                Console.Write($"{n} ");
+            }
+
+            Console.WriteLine();
+
+        }
+
+
+
+    }
+}
---
+++ 2019_07_26_Heap/Heap/Heap/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Heap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tree = { 0, 1, 2, 3, 4, 5, 6};
+
+            Heap heap = new Heap(tree);
+            heap.PrintHeap(); // before
+            heap.Heapify();
+            heap.PrintHeap(); // after
+        }
+    }
+}

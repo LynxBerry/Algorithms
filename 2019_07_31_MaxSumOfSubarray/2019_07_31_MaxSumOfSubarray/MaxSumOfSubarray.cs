4ab6ec37239b13aa4483b2865ecc118f01c44680
Steven Shao <stevensh@microsoft.com>
2019-07-31 17:44:07 +0800

    init

---
+++ 2019_07_31_MaxSumOfSubarray/2019_07_31_MaxSumOfSubarray/MaxSumOfSubarray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2019_07_31_MaxSumOfSubarray
+{
+    class MaxSumOfSubarray
+    {
+        public static int caculateMaxSumOfSubarray(int[] array)
+        {
+            if (array == null || array.Length < 1)
+                throw new Exception("invalid parameters.");
+
+            int curSum = array[0];
+            int curMax = curSum;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (curSum <= 0)
+                    curSum = array[i];
+                else
+                    curSum += array[i];
+
+
+                if (curSum > curMax)
+                    curMax = curSum;
+            }
+
+            return curMax;
+
+        }
+    }
+}
---
+++ 2019_07_31_MaxSumOfSubarray/2019_07_31_MaxSumOfSubarray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2019_07_31_MaxSumOfSubarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxSumOfSubarray.caculateMaxSumOfSubarray(new int[] { 1, 2, 3,-7,8}));
+        }
+    }
+}

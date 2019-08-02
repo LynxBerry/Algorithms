7a932191903c105328ee199d602d4a35b8b98cd1
Steven Shao <stevensh@microsoft.com>
2019-08-02 15:54:16 +0800

    init

---
+++ 2019_08_02_PrintToLargestNdigits/2019_08_02_PrintToLargestNdigits/PrintToLargestNdigits.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2019_08_02_PrintToLargestNdigits
+{
+    class PrintToLargestNdigits
+    {
+
+        public static void Print(int numOfDigits)
+        {
+            // To Do: edge cases;
+
+            PrintCore(new List<int> { }, numOfDigits );
+
+
+        }
+
+        private static void PrintCore(List<int> res, int numOfRestDigits)
+        {
+            if (numOfRestDigits == 0)
+            {
+                PrintResult(res);
+                return;
+            }
+                
+
+            for (int n = 0; n < 10; n++)
+            {
+                List<int> newRes = new List<int>(res); // Any Potential to save space? Good way to clone a List?
+                newRes.Add(n);
+                PrintCore(newRes, numOfRestDigits - 1);
+
+            }
+
+        }
+
+        private static void PrintResult(List<int> res)
+        {
+            // skip zeros
+            int i;
+            for (i = 0; i < res.Count && res[i] == 0; i++)
+            {
+               
+            }
+
+            for (int j = i; j < res.Count; j++)
+            {
+                Console.Write($"{res[j]}");
+            }
+
+            //res.ForEach(ch=> { Console.Write($"{ch}"); });
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ 2019_08_02_PrintToLargestNdigits/2019_08_02_PrintToLargestNdigits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * 题目：输入数字n，按顺序打印出从1最大的n位十进制数。比如输入3，则打印出1、2、3一直到最大的3位数即999。
+ * 
+ */
+
+
+namespace _2019_08_02_PrintToLargestNdigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            PrintToLargestNdigits.Print(100);
+            Console.ReadLine();
+        }
+    }
+}
